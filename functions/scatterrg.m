b1f1ac9e9985da1e105ae0622a29760b65922acd
Bonnaroo <43123441+jakebonney10@users.noreply.github.com>
2023-03-26 15:49:33 -0400

    Update scatterrg.m
    Added comments to function header.

--- functions/scatterrg.m
+++ functions/scatterrg.m
@@ -1,3 +1,18 @@
-function RG = scatterrg(f, c, sigma, theta)
+function [RG] = scatterrg(f, c, sigma, theta)
+
+% [RG] = scatterrg(f, c, sigma, theta)
+%
+%   Scattering and reflection of sound at rough surfaces.
+%   See Clay and Medwin, Acoustical Oceanography 1977 pg. 340
+%
+%   Input: (f(Hz), c(m/s), sigma(m), theta(radians))
+%   Output: scattering loss per bottom bounce
+%
+% Ex.) 
+% theta = (90 - rangle)*pi/180; % convert from incident angle to grazing angle and radians
+% sigma = .01; % 1cm (cobble/sand) % guess and compare model to data
+% loss = scatterrg(f,1500,sigma,theta);
+% lossy_amplitdue = amplitude.*loss.^numbotbnc
+
 k = 2*pi*f./c;
 RG = exp(-2.*k.^2.*sigma.^2.*cos(theta).^2);
